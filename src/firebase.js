f773cd4693b0beb2dce4dbc899766d7042b7feb2
Zenith Zack <faldund@outlook.com>
2022-01-21 23:34:09 -0500

    added firebase.js config file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBjQrkfaz5GvbM0L1dFgoZnsWbxCFLCKTk",
+    authDomain: "netflix-clone-13ab1.firebaseapp.com",
+    projectId: "netflix-clone-13ab1",
+    storageBucket: "netflix-clone-13ab1.appspot.com",
+    messagingSenderId: "112653502808",
+    appId: "1:112653502808:web:9c91f8f2ebdfc67c00bd2d",
+    measurementId: "G-85CPJ505G3"
+  };
